485d1799a33bd885853a5c558faf3ba2520d6d67
AbaddONorOFF <63601124+AbaddONorOFF@users.noreply.github.com>
2020-04-29 21:05:10 +0200

    Update script.js

--- Progress-bar/js/script.js
+++ Progress-bar/js/script.js
@@ -6,12 +6,12 @@
 
 
 const fulfill = e => {
-    const progress = document.getElementById('progress');
-    progress.style.width = parseInt(progress.style.width) + e.target.dataset.value + '%'
-    }
-     
-    let buttons = document.getElementsByTagName('button');
-    [].forEach.call(buttons, (button) => {
-    button.onclick = fulfill;
-    })
+const progress = document.getElementById('progress');
+progress.style.width = (parseInt(progress.style.width) + parseInt(e.target.dataset.value)) + '%'
+}
+ 
+let buttons = document.getElementsByTagName('button');
+[].forEach.call(buttons, (button) => {
+button.onclick = fulfill;
+})
     
